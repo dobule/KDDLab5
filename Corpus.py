7d47b5c23e0c8bf068a9530ae6ae22b916f4fc27
John Torres <jandrewtorres5@gmail.com>
2018-11-16 01:27:12 -0800

    adapted textVectorizer.py to new Corpus and Document classes. Updated Corpus and Document classes.

--- Corpus.py
+++ Corpus.py
@@ -1,10 +1,12 @@
 # Name: Ryan Gelston
 # Class: CSC 466-01 (Fall 2018)
 # Description: Stores and creates a collection of documents
+import os
+from Document import *
+
 
 class Corpus:
-
-    def __init__(self, docs_path, stop_word_path=None, stem_f=None):
+    def __init__(self, docs_path, stop_word_path=None, should_stem=False):
         """ Creates an instance of a corpus
 
               docs_path      -- Path to documents folder
@@ -12,17 +14,52 @@ class Corpus:
               stem_f         -- Stemming function for corpus
         """
 
-        # Go through all documents in docs_path and create Document
-        #   objects for each of them
+        # init hyper params
+        self.should_stem = should_stem
+        self.stop_words = []
+        self.init_stop_words(stop_word_path)
+
+        # init documents
         self.documents = []
+        self.init_docs(docs_path)
 
-        # Find average length of the documents
-        self.avdl = 0
-
-        # Find number of documents in corpus
-        self.n_docs = 0
-
-        # Create the vector shema
+        # Create the vector schema
         self.vect_schema = []
 
+    def init_stop_words(self, stop_word_path):
+        """ initializes stop words """
+        if not stop_word_path:
+            return
 
+        for docname in os.listdir(os.path.abspath(stop_word_path)):
+            with open(os.path.join(os.path.abspath(stop_word_path), docname),
+                      'r') as f:
+                self.stop_words.extend(
+                    [word.strip() for word in f.readlines() if word != '\n'])
+
+    def init_docs(self, rootdir):
+        """ initializes the documents in the corpus """
+        for dir in os.listdir(rootdir):
+            subdir = os.path.join(os.path.abspath(rootdir), dir)
+            for author_dir in os.listdir(subdir):
+                docs_dir = os.path.join(subdir, author_dir)
+                for docname in os.listdir(docs_dir):
+                    with open(os.path.join(os.path.abspath(rootdir), dir,
+                                           author_dir, docname), 'r') as f:
+                        text = f.read()
+                    self.documents.append(
+                        Document(text, author_dir, docname, self.stop_words,
+                                 self.should_stem))
+
+    def n_docs(self):
+        """ returns the number of documents """
+        return len(self.documents)
+
+    def avg_length(self):
+        """ returns the average length of documents """
+        return sum([len(doc.text) for doc in self.documents]) / len(
+            self.documents)
+
+    def ground_truth(self):
+        """ list of (title, author) tuples for all documents """
+        return [(doc.title, doc.author) for doc in self.documents]
--- textVectorizer.py
+++ textVectorizer.py
@@ -1,70 +1,37 @@
-import sys
-import os
-import re
-import string
-import argparse
-from porter import *
+from Corpus import *
+from Document import *
 
+import argparse
+
+# parse arguments
 parser = argparse.ArgumentParser()
+# positional
 parser.add_argument('rootdir', help='root directory of rutgers dataset')
-parser.add_argument('-sw', '--stop-words', action='store_true', help='enable stop word removal')
+# optional
+parser.add_argument('-sw', '--stop-words', action='store', help='stop words file path')
 parser.add_argument('-st', '--stem', action='store_true', help='enable stemming')
 parser.add_argument('--ground-truth', action='store', help='ground truth output filename')
+parser.add_argument('--output', action='store', help='vector output filename')
 args = parser.parse_args()
-
 ROOT_DIR = args.rootdir
-REMOVE_STOP_WORDS = args.stop_words
+STOP_WORDS = args.stop_words
 STEMMING = args.stem
 GT_OUTFILE = args.ground_truth
+OUTPUT = args.output
 
-documents = {}
+# create corpus
+corpus = Corpus(ROOT_DIR, STOP_WORDS, STEMMING)
 
-for dir in os.listdir(ROOT_DIR):
-    subdir = os.path.join(os.path.abspath(ROOT_DIR), dir)
-    for author_dir in os.listdir(subdir):
-        if author_dir not in documents:
-            documents[author_dir] = {}
-        docs_dir = os.path.join(subdir, author_dir)
-        for docname in os.listdir(docs_dir):
-            with open(os.path.join(os.path.abspath(ROOT_DIR), dir, author_dir, docname), 'r') as f:
-                text = f.read()
-            documents[author_dir][docname] = text
-
-stop_words = []
-
-for docname in os.listdir(os.path.abspath('./StopWords')):
-    with open(os.path.join(os.path.abspath('./StopWords'), docname), 'r') as f:
-        stop_words.extend([word.strip() for word in f.readlines() if word != '\n'])
-
-for author in documents.keys():
-    porter = PorterStemmer()
-
-    for document_name in documents[author].keys():
-        document = documents[author][document_name]
-        words = []
-        for word in document.split():
-            word = word.strip().strip(string.punctuation).lower()
-            should_add = True
-            if word == '':
-                should_add = False
-            if not word.isalpha:
-                should_add = False
-            if REMOVE_STOP_WORDS:
-                if word in stop_words:
-                    should_add = False
-            if STEMMING and should_add:
-                word = porter.stem(word, 0, len(word) - 1)
-            if should_add:
-                words.append(word)
-        print(words)
-        documents[author][document_name] = words
-
+# write ground truth to file
 if GT_OUTFILE:
-    ground_truth = []
-    for author in documents.keys():
-        for document_name in documents[author].keys():
-            ground_truth.append((document_name, author))
-    with open('ground_truth.txt', 'w') as f:
-        conv_gt = [','.join(list(entry)) for entry in ground_truth]
+    with open(GT_OUTFILE, 'w') as f:
+        conv_gt = [','.join(list(entry)) for entry in corpus.ground_truth()]
         f.write('\n'.join(conv_gt))
 
+# write vectors to file
+out_string = '\n'.join([doc.vector for doc in corpus.documents])
+if OUTPUT:
+    with open(OUTPUT, 'w') as f:
+        f.write(out_string)
+else:
+    print(out_string)
